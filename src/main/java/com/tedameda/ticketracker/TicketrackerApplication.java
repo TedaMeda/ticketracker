f7e0f64143042b3704db0fe33982731ac1d295bf
TedaMeda <ronakmevada6102@gmail.com>
2024-02-02 11:10:52 +0530

    init commit

---
+++ src/main/java/com/tedameda/ticketracker/TicketrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tedameda.ticketracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tedameda/ticketracker/TicketrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedameda.ticketracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
